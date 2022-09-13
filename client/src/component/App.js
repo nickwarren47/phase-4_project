3372a99cfafdbbb85bbef5096224aedd42f6305d
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-09-13 10:33:18 -0700

    modified

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import logo from '../logo.svg';
+import Reviews from './Reviews';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
+        <Router>
+          <NavBar />
+          <Routes>
+            {/* <Route path="/" element={<Homepage />} />
+            <Route path="/signup" element={<Signup />} /> */}
+            <Route path="/reviews" element={<Reviews />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
