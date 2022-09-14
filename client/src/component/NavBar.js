e48973b9b456523a108a6531ae46c02da973be13
Flordeliza Mae Canlas <105674177+maemaecanlas28@users.noreply.github.com>
2022-09-14 09:03:17 -0700

    Revert "Frontend signin"

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -1,12 +1,6 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-<<<<<<< HEAD
-function NavBar(){
-return(
-    <div>hello</div>
-)
-=======
 
 function Navbar() {
 
@@ -18,7 +12,6 @@ function Navbar() {
             Hello World!
        </div>
     )
->>>>>>> cb06660498cf0c759a187d07e3987d092e47c8d0
 }
 
 export default Navbar
