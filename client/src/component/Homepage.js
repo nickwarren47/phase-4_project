e5f9f767a2cf714887217b81cd560d18c1eef224
Nick Warren <nickwarren47@gmail.com>
2022-09-14 17:00:28 -0600

    Changed homepage

--- client/src/component/Homepage.js
+++ client/src/component/Homepage.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom"
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function Homepage() {
 
     return (
-        <div>
+        <div id='hompage'>
             <div className='bg-[url(https://user-images.githubusercontent.com/106715328/189964202-fef8374d-3e12-4b80-a381-4323525f8ba7.jpg)] h-400 w-full bg-cover bg-center p-20'>
                 <div className="flex items-center justify-center scale-125 p-5">
                     <img src="https://user-images.githubusercontent.com/106715328/188249403-12a9040b-64c2-4069-a05a-21fa8e9a1f6b.png" alt="logo" />
                 </div>
-                <h1 className='text-white text-7xl font-bold mt-12 text-center p-5'>Welcome to Wanderlust!</h1>
-                <h2 className="text-white text-3xl font-light mt-5 text-center"> Your place to find a place</h2>
+                <h1 className='text-sky-500 text-7xl font-bold mt-12 text-center p-5'>Welcome to Wanderlust!</h1>
+                <h2 className="text-sky-500 text-4xl font-light mt-5 text-center"> Your place to find a place</h2>
                 <div className="flex items-center justify-center mt-7 space-x-7">
-                    <TailwindcssButton>
-                        Let's Get Out There!
-                    </TailwindcssButton>
-                    <TailwindcssButton>
-                        <Link to="/login"> User Login </Link>
-                    </TailwindcssButton>
-                    <TailwindcssButton>
-                        <Link to="/signup"> User Sign Up </Link>
-                    </TailwindcssButton>
+                    <div className="flex flex-wrap gap-2">
+                        <TailwindcssButton>
+                            <Link to="/destinations"> Let's Get Out There! </Link>
+                        </TailwindcssButton>
+                        <TailwindcssButton>
+                            <Link to="/login"> User Login </Link>
+                        </TailwindcssButton>
+                        <TailwindcssButton>
+                            <Link to="/signup"> User Sign Up </Link>
+                        </TailwindcssButton>
+                    </div>
                 </div>
             </div>
         </div>
@@ -29,8 +31,7 @@ function Homepage() {
 
     function TailwindcssButton(props) {
         return (
-            <button className="bg-blue-700 text-white text-3xl
-          font-large font-bold px-6 py-4 rounded-lg hover:bg-yellow-600">{props.children}</button>
+            <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-large rounded-lg text-3xl px-6 py-4 text-center mr-2 mb-2">{props.children}</button>
         )
     }
 }
