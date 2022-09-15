32b2c4f479a5571cc0843adff4e13233db51096f
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-09-15 16:22:43 -0700

    trying to fix fetching/routing

--- client/src/component/DestinationCard.js
+++ client/src/component/DestinationCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import { Button } from "flowbite-react"
+import { useNavigate } from 'react-router-dom'
 
-function DestinationCard({ country_or_territory, continent, flag_url, handleDestClick, image_url, reviews, destReviews }) {
+function DestinationCard({ id, country_or_territory, continent, flag_url, handleDestClick, image_url, reviews, destReviews }) {
 
   let navigate = useNavigate()
 
-  function handleDestCardClick() {
-    navigate("/destination-reviews")
+  function handleDestCardClick(destID) {
+    navigate(`/destinations/${destID}`)
   }
 
   return (
@@ -16,7 +16,7 @@ function DestinationCard({ country_or_territory, continent, flag_url, handleDest
       <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{country_or_territory}</h5>
       <p className="font-medium text-black cursor-pointer mb-3 dark:text-gray-400" >{continent}</p>
       <div className="flex flex-wrap gap-2">
-        <Button onClick={handleDestCardClick} gradientMonochrome="success">
+        <Button onClick={() => handleDestCardClick(id)} gradientMonochrome="success">
           Reviews
         </Button>
       </div>
